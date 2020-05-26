45007dbe90025be310a9721a0f8a24555b73d6bf
unknown <hemavathi.mp@ltts.com>
2020-05-26 15:02:18 +0530

    calculator

---
+++ calculator/calc.JavadesktopApp/src/calc/JavadesktopApp/Programs.java
@@ -0,0 +1,12 @@
+package calc.JavadesktopApp;
+
+public class Programs {
+
+	public static void main(String[] args) {
+		
+		 new Calculator();
+		
+		
+	}
+
+}
---
+++ calculator/calc.JavadesktopApp/src/calc/JavadesktopApp/QuitCalculator.java
@@ -0,0 +1,14 @@
+package calc.JavadesktopApp;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class QuitCalculator implements ActionListener {
+
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.exit(0);
+	}
+
+}
